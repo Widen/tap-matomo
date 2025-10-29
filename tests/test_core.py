55394a5563923c4a1c0baca8b1c3815d1123d19a
Vishal-Sharma-Acquia <vishal.sharma@acquia.com>
2025-10-30 00:36:46 +0530

    Remove Python version 3.14 from test.yml and comment out TestTapmatomo in test_core.py

--- tests/test_core.py
+++ tests/test_core.py
@@ -7,16 +7,21 @@ from singer_sdk.testing import get_tap_test_class
 from tap_matomo.tap import Tapmatomo
 
 SAMPLE_CONFIG = {
-    "start_date": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d"),
-    # TODO: Initialize minimal tap config
+    "api_url": "https://demo.matomo.org/",
+    "token_auth": "test_token",
+    "idSite": "1",
+    "method": "VisitsSummary.get",
+    "period": "day",
+    "date": "yesterday",
+    "format": "json",
 }
 
 
 # Run standard built-in tap tests from the SDK:
-#TestTapmatomo = get_tap_test_class(
-#    tap_class=Tapmatomo,
-#    config=SAMPLE_CONFIG,
-#)
+TestTapmatomo = get_tap_test_class(
+    tap_class=Tapmatomo,
+    config=SAMPLE_CONFIG,
+)
 
 
 # TODO: Create additional tests as appropriate for your tap.
